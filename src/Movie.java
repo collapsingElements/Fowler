2cf4770935f09e79c802c3f00103336e1745b0d0
Chris Heizmann <cheizmann@meyle-mueller.de>
2016-05-01 19:28:23 +0200

    Moved method getFrequentRenterPoints to Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -48,4 +48,17 @@ public class Movie
 		}
 		return result;
 	}
+
+	int getFrequentRenterPoints(int daysRented)
+	{
+		// add bonus for a two day new release rental
+		if ((getPriceCode() == Movie.NEW_RELEASE ) && daysRented > 1)
+		{
+			return 2;
+		}
+		else
+		{
+			return 1;
+		}
+	}
 }
--- src/Rental.java
+++ src/Rental.java
@@ -26,14 +26,6 @@ class Rental
 
 	int getFrequentRenterPoints()
 	{
-		// add bonus for a two day new release rental
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE ) && getDaysRented() > 1)
-		{
-			return 2;
-		}
-		else
-		{
-			return 1;
-		}
+		return movie.getFrequentRenterPoints(daysRented);
 	}
 }
