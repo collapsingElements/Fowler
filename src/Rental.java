828bb4b8f9c3f38aa98bc2ca449a53b4f3222a1c
Chris Heizmann <cheizmann@meyle-mueller.de>
2016-05-01 17:49:42 +0200

    Extracted calculation of frequentRenterPoints and moved it to Rental class

--- src/Rental.java
+++ src/Rental.java
@@ -40,4 +40,17 @@ class Rental
 		}
 		return result;
 	}
+
+	int getFrequentRenterPoints()
+	{
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE ) && getDaysRented() > 1)
+		{
+			return 2;
+		}
+		else
+		{
+			return 1;
+		}
+	}
 }
